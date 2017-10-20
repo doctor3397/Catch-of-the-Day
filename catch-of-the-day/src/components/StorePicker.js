aee8996868f0b010e5f48d15fb4cb1e06d70f804
Theresa Shen <tshen@architech.ca>
2017-10-19 23:41:17 -0400

    Add onClick event listener

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,14 +2,25 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  // constructor() {
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+  goToStore(event) {
+    // Stop the form from submiting/ reloading the page
+    event.preventDefault();
+    // Grab the text from the box
+    console.log(this.storeInput.value);
+    // Transition from / to /store/:storeId
+  }
+
   render() {
-    return (
-      <form className="store-selector">
+    return (                           /* onSubmit = {this.goToStore.bind(this)} */
+      <form className="store-selector" onSubmit={ e => this.goToStore(e)} >
         <h2>Please Enter A Store</h2>
         <input type="text" required placeholder="Store Name"
-        defaultValue={getFunName()}/>
+        defaultValue={getFunName()} ref={(input) => {this.storeInput = input} } />
         <button type="submit">Visit Store</button>
-
       </form>
     )
   }
