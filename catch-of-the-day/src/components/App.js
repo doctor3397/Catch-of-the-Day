016facc87d42179ca52cffca09b1834afd15aee0
Theresa Shen <tshen@architech.ca>
2017-10-23 23:11:12 -0400

    Create an initial states for fishes and orders. Pass the addFish function to the addFishForm

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -3,7 +3,29 @@ import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 
-class App extends React.Component {
+class App extends React.Component{
+  constructor() {
+    super();
+    this.addFish = this.addFish.bind(this);
+    // Initial state
+    this.state = {
+      fishes: {},
+      order: {}
+    }
+  }
+
+  addFish(fish) {
+    // update the state
+    // Take a copy of the existing state ...spread
+    const fishes = {...this.state.fishes};
+    // Add in the new fish
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    // this.state.fishes.fish1 = fishe1;
+    // set the state
+    this.setState({fishes: fishes});
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +33,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     )
   }
