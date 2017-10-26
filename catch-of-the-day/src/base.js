3b5c8ff4a30b55f0f061027f2940e8f5e7b8fe63
Theresa Shen <tshen@architech.ca>
2017-10-26 15:35:39 -0400

    Use Firebase, componentWillMount() and componentWillUnmount() to persist database

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,10 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  // Initialize Firebase
+  apiKey: "AIzaSyBWEWrWABOujaHeDOQm5b9K_Tg6CHhjzrI",
+  authDomain: "catch-of-the-da-4312a.firebaseapp.com",
+  databaseURL: "https://catch-of-the-da-4312a.firebaseio.com",
+});
+
+export default base;
